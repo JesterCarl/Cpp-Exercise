56ceee5b275ec01652fa892b248a3e327ac82787
JesterCarl <146396074+JesterCarl@users.noreply.github.com>
2023-10-02 13:59:40 +0800

    Add files via upload

---
+++ Sample 11 Jc.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	
+    int x;	
+	
+	cout<<"Enter your grade:";
+	cin>>x;
+	
+	if (x>=75){	
+		cout<< "Passed";
+	}else{	
+		cout<< "Failed";	
+	}
+	return 0;
+}
