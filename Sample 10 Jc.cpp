7f0d8655050018fea292e84e3005a95110a11f1a
JesterCarl <146396074+JesterCarl@users.noreply.github.com>
2023-10-02 13:52:32 +0800

    Add files via upload

---
+++ Sample 10 Jc.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	
+int x;
+
+cout<<"Enter your age:";
+cin>>x;
+
+if(x>=18){
+	
+	cout<< " You are in a Legal age\n";
+	cout<< "Pwede ka eon Makulong";
+}else{
+	
+	cout << " You are a minor";
+	
+	
+}
+
+	
+	
+	
+	
+	return 0;
+}
